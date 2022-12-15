3679dfef5a1d216f55a3f858126302d36d0e3bb2
navaneeth38 <117112296+navaneeth38@users.noreply.github.com>
2022-12-15 20:58:52 +0530

    Update Home.js

--- to_do/src/components/Home.js
+++ to_do/src/components/Home.js
@@ -1,6 +1,6 @@
-import React,{useState,useEffect} from 'react'
-import {useLocation} from 'react-router-dom'
+import React,{useState} from 'react'
 import Display from './Display'
+
 
 function Home (){
     const [todos,setTodos] = useState([])
@@ -8,11 +8,17 @@ function Home (){
      const [value,setValue]=useState("")
      const [prior,setPrior]=useState(1)
      
-     const submitHandler = (subject,date,priority) =>{
-        setTodos([...todos,{subject: text,date: value,priority: prior}])
-        setText("")
-        setValue("")
-        setPrior(0)
+     const submitHandler = () =>{
+        if(text ===""){
+        console.log("ERROR")
+        }
+        else{
+         setTodos([...todos,{subject: text,date: value,priority: prior}])
+         setText("")
+         setValue("")
+         setPrior(0)
+        }
+       
      }
      const deleteTask=(index)=>{
     
@@ -21,7 +27,7 @@ function Home (){
         setTodos(itemCopy)
       }
     
-     const setTask = (item,index) =>{
+     const setTask = (item) =>{
         setText(item.subject)
         setValue(item.value)
         setPrior(item.priority)
@@ -30,10 +36,10 @@ function Home (){
     
   return( 
   <div >
-    <div className="container">
+    <div className='section'>
       {todos.map((item,index)=>{
         return(
-            <div key={index} >
+            <div key={index} className="main" >
                 <Display text={item}/>
                 <button onClick={()=> setTask(item,index)}>Modify</button>
                 <button onClick={()=> {deleteTask(index)}}>X</button>
@@ -41,8 +47,8 @@ function Home (){
         )})}
     </div>
     <div className='input'>
-     <input name="subject" type="text" value={text} onChange={(e)=>setText(e.target.value)} />  
-     <input name="date"  type="date" value={value} onChange={(e)=>setValue(e.target.value)}/>  
+     <input name="subject" type="text" value={text} onChange={(e)=>setText(e.target.value)} /> 
+     <input name="date" type="date"  value={value} onChange={e=>setValue(e.target.value)}/>  
      <input name='priority' type='number' min={0} max={3} value={prior} onChange={(e)=>setPrior(e.target.value)} />
      <button onClick={()=>submitHandler()}>Add</button>
     </div>  
