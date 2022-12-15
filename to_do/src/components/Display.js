54dba356f99f638a74d8d15e3c31cee7348481b3
navaneeth38 <117112296+navaneeth38@users.noreply.github.com>
2022-12-15 21:00:02 +0530

    Update Display.js

--- to_do/src/components/Display.js
+++ to_do/src/components/Display.js
@@ -1,19 +1,31 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
+
 
 const Display = (props) => {
-    const [done,setDone] = useState(false)
-    const item = props.text
-    return (
-        <div>
-            <div  className="container" style={{textDecoration: (done) && 'line-through'}}>
-                <input type="checkbox" checked={done} onChange={e => setDone(e.target.checked)} />
-                <span className='sub'>{item.subject} </span>
-                <span className='sub'>{item.date}</span>
-                <span className='sub'>{(item.priority == 1) ? "High" : (item.priority ==2 ? "Medium" : "low") }</span>
-                
-            </div>
-        </div>
-    );
-}
+  const [done, setDone] = useState(false);
+  const item = props.text;
+  return (
+      <div
+        className="container"
+        style={{ textDecoration: done && "line-through" }}
+      >
+        <input
+          type="checkbox"
+          checked={done}
+          onChange={(e) => setDone(e.target.checked)}
+        />
+        <div className="sub">
+          {item.subject}{" "}</div>
+          <span className="sub">Priority: 
+            {item.priority === 1
+              ? "High"
+              : item.priority === 2
+              ? "Medium"
+              : "Low"}
+              </span>
+            <span className="sub">{item.date}</span>
+      </div>
+  );
+};
 
 export default Display;
